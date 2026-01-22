9cc708547defe808515cba3fdda6006747c7c2d2
Daniel Qian <177599677+DanielQ13@users.noreply.github.com>
2026-01-23 05:30:05 +0800

    Add Galath remove Duke

---
+++ src/main/java/Galath.java
@@ -0,0 +1,10 @@
+public class Galath {
+    public static void main(String[] args) {
+        printMessage("Hello! I'm Galath\n" + "What can I do for you?");
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+}

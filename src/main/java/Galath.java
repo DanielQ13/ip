dd37453afed0d3d25a1b2fd231b414a8073bc48d
Daniel Qian <177599677+DanielQ13@users.noreply.github.com>
2026-01-23 05:49:10 +0800

    Add echos

--- src/main/java/Galath.java
+++ src/main/java/Galath.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Galath {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         printMessage("Hello! I'm Galath\n" + "What can I do for you?");
-        printMessage("Bye. Hope to see you again soon!");
+
+        String command;
+        while (true) {
+            command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                printMessage("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                printMessage(command);
+            }
+        }
+
+        scanner.close();
     }
 
     private static void printMessage(String message) {
-        System.out.println(message);
+        System.out.println("    " + message);
     }
 }

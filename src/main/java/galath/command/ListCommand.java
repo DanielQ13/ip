1a330780eb66d3eebef026438bfb3e4c11629de4
Daniel <danielqian64@gmail.com>
2026-02-18 23:01:54 +0800

    Add representative screenshot

--- src/main/java/galath/command/ListCommand.java
+++ src/main/java/galath/command/ListCommand.java
@@ -26,7 +26,7 @@ public class ListCommand extends Command {
         }
 
         StringBuilder message = new StringBuilder("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
+        for (int i = 0; i < tasks.size (); i++) {
             message.append("\n     ").append(i + 1).append(". ").append(tasks.getTasks().get(i));
         }
         ui.showMessage(message.toString());
